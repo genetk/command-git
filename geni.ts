0e269f14802b7e2f609353b20037c96319157c7e
genetk <151810253+genetk@users.noreply.github.com>
2023-11-27 23:53:21 +0000

    clsno36

--- geni.ts
+++ geni.ts
@@ -2,4 +2,5 @@ console.log("it is genet k");
  
 console.log("i am msd student");
 
-console.log("i am taking asynchronus course with proffesor assad");
+console.log("i am taking asynchronus course with proffesor Assad");
+console.log ("class no 36")
