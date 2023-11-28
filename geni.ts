1a8185d9e640374ef33782f4897dedbb712db6bc
genetk <151810253+genetk@users.noreply.github.com>
2023-11-28 00:01:45 +0000

    fixed

--- geni.ts
+++ geni.ts
@@ -3,6 +3,7 @@ console.log("it is genet k");
 console.log("i am msd student");
 
 console.log("i am taking asynchronus course with proffesor Assad");
-console.log ("class no 36")
+console.log ("class no 36");
 
-console.log("clas in library")
+console.log("clas in library");
+console.log("fixed heater");
